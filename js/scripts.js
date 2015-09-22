b54b481ee12460de63fb9a436ae69bd21a7bf189
Perry Eising & Diana Holland <student@epicodus.com>
2015-09-22 15:49:08 -0700

    Add behavior for adding and withdrawing funds

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,37 @@
 function BankAccount(name, deposit) {
   this.name = name;
   this.deposit = deposit;
+  return name + " " + deposit;
+}
+
+BankAccount.prototype.withdraw = function(amount) {
+  this.deposit -= amount;
+}
+
+BankAccount.prototype.addfunds = function(amount) {
+  this.deposit += amount;
 }
 
 $(document).ready(function() {
+
 
   $("form#new-bank-account").submit(function(event) {
     event.preventDefault();
 
     var inputtedName = $("input#name").val();
-    var inputtedDeposit = parseInt($("input#deposit").val());
-    var newBankAccount = new BankAccount(inputtedName, inputtedDeposit);
-    console.log(newBankAccount);
+    var inputtedDeposit = parseInt($("input#deposit-form").val());
+    if (inputtedDeposit < 20) {
+      $(".minimumMessage").show();
+    } else {
+      var newBankAccount = new BankAccount(inputtedName, inputtedDeposit);
 
-    $("p#welcome").append("Welcome " + newBankAccount.name + "<br><br>" + "Your current funds are: " + newBankAccount.deposit);
+      $("p#welcome").append("<br><br>You are " + newBankAccount.name + "<br><br>" + "and you have $" + newBankAccount.deposit);
 
+      $("form#new-bank-account").remove();
+      $("#headline").replaceWith("<h2>Welcome " + newBankAccount.name + "</h2>");
+      $("#manage-account").show();
+    }
   });
+
+
 });
